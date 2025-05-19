248a7691cbe5a7fa1a8f839c67b471a607c43ddd
govindongit <govindnag5863@gmail.com>
2025-05-19 13:16:33 +0530

    angular component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,16 +1,37 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { HeaderComponent } from "./components/header/header.component";
+import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, HomeComponent],
   template: `
+
+  <app-header/>
+  <main>
+    <app-home/>
+  </main>
+
+
     <h1>Welcome to {{title}}!</h1>
+    <p>
+      Hello world
+    </p>
 
     <router-outlet />
   `,
-  styles: [],
+  styles: [`
+
+  main{
+    padding:16px;
+  }
+   p{
+    background-color:red;
+   }
+   
+    `],
 })
 export class AppComponent {
-  title = 'first-ng-app';
+  title = 'My Angular learning journey';
 }
