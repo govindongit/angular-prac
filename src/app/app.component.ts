b597082edbf9f035808c62601e158a86350e9d3b
govindongit <govindnag5863@gmail.com>
2025-05-20 18:32:38 +0530

    Router

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,35 +1,28 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { HeaderComponent } from "./components/header/header.component";
-import { HomeComponent } from "./home/home.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet, HeaderComponent, HomeComponent],
+  imports: [RouterOutlet, HeaderComponent],
   template: `
 
   <app-header/>
   <main>
-    <app-home/>
+    <router-outlet/>
   </main>
+  
+  <h1>Welcome to {{title}}!</h1>
+  <p>Hello world</p>
 
-
-    <h1>Welcome to {{title}}!</h1>
-    <p>
-      Hello world
-    </p>
-
-    <router-outlet />
   `,
   styles: [`
-
   main{
     padding:16px;
   }
-   p{
+  p{
     background-color:red;
-   }
-   
+  }
     `],
 })
 export class AppComponent {
